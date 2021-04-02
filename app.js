d7d342df8b9fd5aabe9d7c28816219759b2b9027
COMP229-F2020-300881481 <sokonofu@my.centennialcollege.ca>
2021-04-01 20:02:01 -0400

    Setup mongo session store

--- app.js
+++ app.js
@@ -4,71 +4,59 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const expressLayouts = require("express-ejs-layouts");
-const session = require("express-session");
 const flash = require("connect-flash");
-const db = require("./db");
 const router = require("./routers");
+const useSession = require("./middleware/session");
 const usePassport = require("./middleware/passport");
 const setViewGlobals = require("./middleware/setViewGlobals");
 
-db.connect();
+module.exports = () => {
+  const app = express();
 
-const app = express();
+  // view engine setup
+  app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "ejs");
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+  // layout engine setup
+  app.use(expressLayouts);
+  app.set("layout", path.join(__dirname, "views/layouts/index"));
 
-// layout engine setup
-app.use(expressLayouts);
-app.set("layout", path.join(__dirname, "views/layouts/index"));
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "public")));
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+  // Session setup
+  useSession(app);
 
-app.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
-});
+  // Initialize flash
+  app.use(flash());
 
-//express Session setup
-app.use(
-  session({
-    secret: "SomeSecret",
-    saveUninitialized: false,
-    resave: false,
-  })
-);
+  // Initialize passport
+  usePassport(app);
 
-// Initialize flash
-app.use(flash());
+  // Attach view globals middleware
+  app.use(setViewGlobals);
 
-//Initialize passport
-usePassport(app);
+  // Initialize router
+  app.use(router);
 
-// Attach view globals middleware
-app.use(setViewGlobals);
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-// Initialize router
-app.use(router);
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-module.exports = app;
+  return app;
+};
